5d0cb15f00b10466ecc00fd615511f2c12764fd1
Jeff Burke <jburke@ucla.edu>
2015-01-31 14:51:41 -0800

    updates to integrate ndn

--- src/bmspublisher.py
+++ src/bmspublisher.py
@@ -4,8 +4,21 @@ import parse # https://github.com/r1chardj0n3s/parse
 import argparse, sys, json
 from datetime import datetime
 
-def publish(logline, rootname):
-    
+try:
+    import asyncio
+except ImportError:
+    import trollius as asyncio
+
+from pyndn import Name, Data, ThreadsafeFace
+from pyndn.security import KeyChain
+from pyndn.security.identity import FilePrivateKeyStorage, BasicIdentityStorage
+from pyndn.security.identity import IdentityManager
+from pyndn.util.memory_content_cache import MemoryContentCache
+
+
+
+def publish(logline, rootname, cache):
+    global face, keychain
     # Pull out and parse datetime for log entry 
     # (note we shoudld use point time for timestamp)
     try:
@@ -25,9 +38,18 @@ def publish(logline, rootname):
     data_json, data_dict = pointToJSON(point)
     
     if name is not None:
-        print(logdt, name, data_dict["timestamp"], data_json)
-        
-                            
+        print("Publishing log entry", logdt, "to", name, data_dict["timestamp"], "payload:", data_json)
+        try:
+            cache.add(createData(name, data_dict["timestamp"], data_json))
+        except Exception as detail:
+            print("publish: Error calling createData for", logline, "-", detail)
+
+def createData(name, timestamp, payload):
+    data = Data( Name("ndn:"+name+"/"+str(timestamp)) ) 
+    data.setContent(payload)
+    keychain.sign(data, keychain.getDefaultCertificateName())
+    #print(repr(data))
+    return data
 
 def pointNameToName(point, root):
     try: 
@@ -63,8 +85,22 @@ def pointToJSON(pd):
         return "{}"
     return djs, d
 
+@asyncio.coroutine
+def readfile(filename, namespace, cache):
+    f = open(filename, 'r')
+    for line in f:
+        publish(line, namespace, cache)
+    f.close()
+    
+@asyncio.coroutine
+def followfile(filename, namespace):
+    for line in tailer.follow(open(filename)):
+        publish(line, namespace, cache)
+
+        
 def main(): 
   
+    # COMMAND LINE ARGS
     parser = argparse.ArgumentParser(description='Parse or follow Cascade Datahub log and publish to NDN.')
     parser.add_argument('filename', help='datahub log file')
     parser.add_argument('-f', dest='follow', action='store_true',
@@ -73,14 +109,21 @@ def main():
                         help='root ndn name, no trailing slash')
     args = parser.parse_args()
   
+    # NDN 
+    global face, keychain
+    loop = asyncio.get_event_loop()
+    face = ThreadsafeFace(loop, "localhost")
+
+    keychain = KeyChain(IdentityManager(BasicIdentityStorage(), FilePrivateKeyStorage() ))       # override default even for MacOS
+    cache = MemoryContentCache(face)
+  
+    # READ THE FILE (MAIN LOOP)
     if args.follow: 
-        for line in tailer.follow(open(args.filename)):
-            publish(line, args.namespace)
+        loop.run_until_complete(followfile(args.filename, args.namespace, cache))
     else:
-        f = open(args.filename, 'r')
-        for line in f:
-            publish(line, args.namespace)
-        f.close()
+        loop.run_until_complete(readfile(args.filename, args.namespace, cache))
+
+    face.shutdown()
         
 if __name__ == '__main__':
     main()
